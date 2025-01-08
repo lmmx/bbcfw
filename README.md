66ca0041ecca84a2b343790076f0c45b5719e1b4
Louis Maddox <louismmx@gmail.com>
2025-01-08 00:44:20 +0000

    feat: prepare to carry out the data processing in miniature

--- README.md
+++ README.md
@@ -3,4 +3,33 @@
 Exploring the BBC News subset of the FineWeb dataset (via HuggingFaceFW/fineweb's dated subsets on HF),
 originally a Common Crawl dataset. Iterating on a previous use of the C4 dataset [here](https://github.com/lmmx/bbcc4).
 
+## Speed benchmarking
 
+The sample 10BT (14 files, each 2.15GB = 30.1GB, only loading 3 columns: url, text, language)
+
+- Each file takes about 45 seconds
+- There are ~25,000 files in the other ~100 non-sample subsets, which suggests ~13 days estimated processing time.
+
+```
+0it [00:00, ?it/s]Processing hf://datasets/HuggingFaceFW/fineweb/sample/10BT/000_00000.parquet
+1it [00:41, 41.21s/it]Processing hf://datasets/HuggingFaceFW/fineweb/sample/10BT/001_00000.parquet
+2it [01:26, 43.58s/it]Processing hf://datasets/HuggingFaceFW/fineweb/sample/10BT/002_00000.parquet
+3it [02:10, 43.65s/it]Processing hf://datasets/HuggingFaceFW/fineweb/sample/10BT/003_00000.parquet
+4it [02:52, 43.32s/it]Processing hf://datasets/HuggingFaceFW/fineweb/sample/10BT/004_00000.parquet
+5it [03:40, 44.68s/it]Processing hf://datasets/HuggingFaceFW/fineweb/sample/10BT/005_00000.parquet
+6it [04:26, 45.31s/it]Processing hf://datasets/HuggingFaceFW/fineweb/sample/10BT/006_00000.parquet
+7it [05:11, 45.06s/it]Processing hf://datasets/HuggingFaceFW/fineweb/sample/10BT/007_00000.parquet
+8it [05:53, 44.24s/it]Processing hf://datasets/HuggingFaceFW/fineweb/sample/10BT/008_00000.parquet
+9it [06:41, 45.47s/it]Processing hf://datasets/HuggingFaceFW/fineweb/sample/10BT/009_00000.parquet
+10it [07:25, 44.95s/it]Processing hf://datasets/HuggingFaceFW/fineweb/sample/10BT/010_00000.parquet
+11it [08:08, 44.41s/it]Processing hf://datasets/HuggingFaceFW/fineweb/sample/10BT/011_00000.parquet
+12it [09:04, 47.78s/it]Processing hf://datasets/HuggingFaceFW/fineweb/sample/10BT/012_00000.parquet
+13it [09:47, 46.53s/it]Processing hf://datasets/HuggingFaceFW/fineweb/sample/10BT/013_00000.parquet
+14it [10:32, 46.04s/it]Processing hf://datasets/HuggingFaceFW/fineweb/sample/10BT/014_00000.parquet
+15it [10:54, 43.61s/it]
+Creating parquet from Arrow format: 100%|█████████████████████████████████████| 10/10 [00:00<00:00, 120.79ba/s]
+Uploading the dataset shards: 100%|██████████████████████████████████████████████| 1/1 [00:02<00:00,  2.15s/it]
+```
+
+- LazyFrame with `scan_parquet`/`sink_parquet` seems to make it marginally faster (but not tested
+  extensively), I decided to use it regardless as it should reduce the memory load.
--- src/bbcfw/main.py
+++ src/bbcfw/main.py
@@ -1,8 +1,10 @@
 from functools import partial
+from pathlib import Path
 
 import polars as pl
 from datasets import Dataset, load_dataset_builder
-from huggingface_hub import hf_hub_url, list_repo_files, login
+from datasets.exceptions import DatasetNotFoundError
+from huggingface_hub import login
 from tqdm import tqdm
 
 from bbcfw.core.caching import make_cache_path, mktemp_cache_dir
@@ -17,6 +19,7 @@ dataset_id = "HuggingFaceFW/fineweb"
 dataset_id_slug = dataset_id.replace("/", "_")
 username = "permutans"
 result_dataset_name = "fineweb-bbc-news"
+result_dataset_id = f"{username}/{result_dataset_name}"
 
 # 2) Make a directory to cache our transformations of the entire dataset (all subsets)
 
@@ -24,7 +27,6 @@ cache_dir = mktemp_cache_dir(id_path=dataset_id)
 dataset_cache_path = partial(make_cache_path, cache_dir=cache_dir)
 
 parquet_cache_names = cache_dir / f"{dataset_id_slug}_filenames.parquet"
-subset_name = "sample-10BT"  # TODO: fold over all subsets
 
 if parquet_cache_names.exists():
     source_files = pl.read_parquet(parquet_cache_names)
@@ -38,36 +40,69 @@ aggregator = pl.DataFrame(schema=fwnews_features)
 domain_col = pl.col("url").str.extract(domain_capture)
 path_col = pl.col("url").str.extract(subpage_capture)
 
-hf_urls = source_files.select(url=f"hf://datasets/{dataset_id}/" + pl.col("name"))
-pq_caches = list(map(dataset_cache_path, hf_urls["url"]))
+config_names = source_files["config_name"].unique().sort()
 
-for source_url, parquet_cache_chunk in tqdm(zip(hf_urls["url"], pq_caches)):
-    if parquet_cache_chunk.exists():
-        news_df = pl.read_parquet(parquet_cache_chunk)
+
+def ds_subset_exists(dataset_id: str, subset_name: str) -> bool:
+    """Check that the dataset exists, and if so whether the config name is in it."""
+    try:
+        configs = load_dataset_builder(dataset_id).builder_configs
+    except DatasetNotFoundError:
+        print(f"The dataset {dataset_id} was not found.")
+        return False
     else:
-        print(f"Processing {source_url}")
-        # Drop query parameters if ? in URL, drop any non-BBC News domain URLs
-        df = (
-            pl.read_parquet(source_url, columns=["url", "text", "language"])
-            .filter(pl.col("language") == "en")
-            .drop("language")
-            .with_columns(pl.col("url").str.extract(r"([^?]+)"))
-            .filter(
-                domain_col.str.contains(domain_match),
-                ~pl.col("url").str.contains(r"https?://[^/]+\/\?"),  # Path is not `/?`
+        print(f"Configs: {list(configs)}")
+        return subset_name in list(configs)
+
+
+for subset_name in tqdm(config_names[:10]):
+    # Skip any existing subsets entirely
+    if ds_subset_exists(dataset_id=result_dataset_id, subset_name=subset_name):
+        print(f"Skipping {subset_name} as it exists")
+        continue
+    else:
+        print(f"The subset {subset_name} doesn't exist, creating it")
+    hf_urls = (
+        source_files.filter(pl.col("config_name") == subset_name)
+        .select(url=f"hf://datasets/{dataset_id}/" + pl.col("name"))
+        .head(2)
+    )
+    pq_caches = []
+
+    def process_subset_chunk(source_url: str) -> Path:
+        parquet_cache_chunk = dataset_cache_path(source_url)
+        if parquet_cache_chunk.exists():
+            news_df = pl.read_parquet(parquet_cache_chunk)
+        else:
+            print(f"Processing {source_url}")
+            # Drop query parameters if ? in URL, drop any non-BBC News domain URLs
+            news_df = (
+                pl.scan_parquet(source_url, parallel="prefiltered")
+                .select("url", "text", "language")
+                .filter(pl.col("language") == "en")
+                .select(pl.col("url").str.extract(r"([^?]+)"), "text")
+                .filter(
+                    domain_col.str.contains(domain_match),
+                    ~pl.col("url").str.contains(
+                        r"https?://[^/]+\/\?"
+                    ),  # Path is not `/?`
+                )
+                .filter(domain_col.str.contains("news").or_(path_col == "/news/"))
             )
-        )
-        # Just match the "/news/" path here
-        news_df = df.filter(domain_col.str.contains("news").or_(path_col == "/news/"))
-        news_df.write_parquet(parquet_cache_chunk)
+            news_df.sink_parquet(parquet_cache_chunk)
+        return parquet_cache_chunk
 
-# Reload once all parts completed and upload
-aggregator = pl.read_parquet(pq_caches)
+    for url in list(hf_urls["url"]):
+        parquet_cache_chunk = process_subset_chunk(url)
+        pq_caches.append(parquet_cache_chunk)
 
-news_data = aggregator.to_dict(as_series=False)
-news_dataset = Dataset.from_dict(news_data)
-news_dataset.push_to_hub(
-    f"{username}/{result_dataset_name}",
-    config_name=subset_name,
-    private=False,
-)
+    # Reload once all parts completed and upload
+    aggregator = pl.read_parquet(pq_caches)
+
+    news_data = aggregator.to_dict(as_series=False)
+    news_dataset = Dataset.from_dict(news_data)
+    news_dataset.push_to_hub(
+        result_dataset_id,
+        config_name=subset_name,
+        private=False,
+    )
